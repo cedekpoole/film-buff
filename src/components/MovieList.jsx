f76cbcfdb9e72172e595709b6e15a2f9805deaa6
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-07 14:22:03 +0100

    change app name + create minimise button

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,12 @@ export default function MovieList({ movies }) {
   return (
     <>
       <div className="mx-auto flex flex-col rounded bg-slate-900 p-4 gap-2">
-        <h2 className="font-oswald text-2xl">Results</h2>
+        <div className="flex justify-between items-center">
+          <h2 className="font-oswald text-2xl">Results</h2>
+          <button className="text-3xl px-1 rounded-full bg-slate-800 hover:bg-slate-700">
+            -
+          </button>
+        </div>
         {movies.map((movie) => (
           <MovieCard key={movie.imdbID} movie={movie} />
         ))}
