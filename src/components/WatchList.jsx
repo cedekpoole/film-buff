68b5ffaa40149a9ce5df26ceab403470e485176b
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-07 18:31:27 +0100

    add title to watch list

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -7,12 +7,15 @@ export default function WatchList({ watched }) {
       <div className="bg-slate-900 mx-auto flex flex-col rounded p-4 w-full lg:w-1/2">
         <MovieAverages watched={watched} />
         <div className="flex flex-col">
-          {watched.map((watchedMovie) => (
-            <WatchedCard
-              key={watchedMovie.imdbID}
-              watchedMovie={watchedMovie}
-            />
-          ))}
+          <h2 className="font-oswald text-2xl p-4 pb-2">Movies Watched</h2>
+          <div>
+            {watched.map((watchedMovie) => (
+              <WatchedCard
+                key={watchedMovie.imdbID}
+                watchedMovie={watchedMovie}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
