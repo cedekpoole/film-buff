481b1d4807268525934560b1f5c6fea057e1504d
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-07 13:49:41 +0100

    add watched card component + map over watched movies

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
     <>
       <div className="min-h-screen bg-slate-800 text-gray-100 font-quattrocento">
         <Navbar movies={movies} />
-        <main className="container mx-auto p-4 flex">
+        <main className="container mx-auto p-4 flex flex-col md:flex-row gap-2">
           <MovieList movies={movies} />
           <WatchList watched={watched} />
         </main>
--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,10 +1,19 @@
 import MovieAverages from "./MovieAverages";
+import WatchedCard from "./WatchedCard";
 
 export default function WatchList({ watched }) {
   return (
     <>
       <div className="bg-slate-900 mx-auto flex flex-col rounded">
         <MovieAverages watched={watched} />
+        <div className="flex flex-col">
+          {watched.map((watchedMovie) => (
+            <WatchedCard
+              key={watchedMovie.imdbID}
+              watchedMovie={watchedMovie}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
