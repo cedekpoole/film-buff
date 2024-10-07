a7a2af57e3247c817579d715edca6d593281b66b
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-07 12:47:26 +0100

    change icon on differing viewports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,29 @@
 import Icon from "../assets/movie-buff.svg";
+import IconSmall from "../assets/icon-small.svg";
 
 export default function Navbar() {
   return (
     <nav className="bg-slate-900 p-4">
       <div className="container mx-auto flex items-center justify-between">
-        <img src={Icon} alt="Movie Buff Logo" className="w-28" />
+        <div>
+          <img
+            src={Icon}
+            alt="Movie Buff Logo"
+            className="w-28 hidden md:block"
+          />
+          <img
+            src={IconSmall}
+            alt="Movie Buff Logo"
+            className="w-16 block md:hidden"
+          />
+        </div>
         <input
           type="text"
           placeholder="Search for movies..."
-          className="p-2 w-1/3 rounded-md"
+          className="p-2 w-1/2 md:w-1/3 bg-gray-100 rounded-md"
         />
-        <p className="text-white text-xl font-thin">
-          Found <span className="font-bold">X</span> Results
+        <p className="text-gray-100 text-sm md:text-xl font-thin">
+          Found <span className="font-bold text-gray-500">X</span> Results
         </p>
       </div>
     </nav>
