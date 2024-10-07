3e209307df4117cb1e8054a6b061da2fd78b0588
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-07 12:28:44 +0100

    add search bar to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,11 @@ export default function Navbar() {
     <nav className="bg-slate-900 p-4">
       <div className="container mx-auto flex items-center justify-between">
         <img src={Icon} alt="Movie Buff Logo" className="w-28" />
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="p-2 w-1/3 rounded-md"
+        />
         <p className="text-white text-xl font-thin">
           Found <span className="font-bold">X</span> Results
         </p>
