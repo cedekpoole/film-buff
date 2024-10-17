ac650b5355c7add3c2c003b1acf1ae1e25ec3c88
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-17 18:19:50 +0100

    retrieve movie details within movie details component

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+
+const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+
 export default function MovieDetails({ selectedID, onCloseDetails }) {
+  useEffect(() => {
+    async function getMovieDetails() {
+      try {
+        const res = await fetch(
+          `https://www.omdbapi.com/?apikey=${apiKey}&i=${selectedID}`
+        );
+        if (!res.ok) throw new Error("Could not retrieve movie details");
+
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getMovieDetails();
+  }, [selectedID]);
+
   return (
     <div className="bg-slate-900 mx-auto flex justify-between items-start rounded p-4 w-full lg:w-1/2">
       <h3 className="text-xl">Selected Movie ID: {selectedID}</h3>
