7a493b6472fe78b95798c1c09df74f7dfb89271d
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-17 17:37:07 +0100

    fix responsiveness of film card image

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -8,7 +8,7 @@ FilmCard.propTypes = {
 export default function FilmCard({ movie, isWatched }) {
   return (
     <div className="bg-slate-800 shadow-md rounded-md p-4 flex gap-3 hover:bg-slate-700 cursor-pointer min-h-40">
-      <img src={movie.Poster} alt={movie.Title} className="w-1/6 rounded-md" />
+      <img src={movie.Poster} alt={movie.Title} className="w-24 rounded-md" />
       <div className="">
         <h2 className="text-lg font-bold mt-2">{movie.Title}</h2>
         <p className="text-gray-300">{movie.Year}</p>
