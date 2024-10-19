96d64e2c7b0f416251af28522ff2eed1eeae6226
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-19 16:31:27 +0100

    fix styling of button component

--- src/components/UI/MinimiseBtn.jsx
+++ src/components/UI/MinimiseBtn.jsx
@@ -1,7 +1,7 @@
 export default function MinimiseBtn({ isOpen, setIsOpen }) {
   return (
     <button
-      className="text-3xl px-1 rounded-full bg-slate-800 hover:bg-slate-700"
+      className="text-3xl px-1 rounded-full bg-slate-800 hover:bg-slate-700 self-start"
       onClick={() => setIsOpen((open) => !open)}
     >
       {isOpen ? `-` : `+`}
