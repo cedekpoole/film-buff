b29a636d4325d87cd6987ff540e14686f745624b
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-19 13:29:02 +0100

    improve film card styling

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -11,11 +11,15 @@ export default function FilmCard({ movie, isWatched, onSelectedID, onDelete }) {
   return (
     <div
       className={`bg-slate-800 shadow-md rounded-md p-4 flex gap-3 ${
-        !isWatched && `hover:bg-slate-700 cursor-pointer`
-      } min-h-40`}
+        !isWatched && `hover:bg-slate-700 cursor-pointer min-h-28`
+      }`}
       onClick={() => onSelectedID(imdbID)}
     >
-      <img src={Poster} alt={Title} className="w-24 rounded-md" />
+      <img
+        src={Poster}
+        alt={Title}
+        className={`${!isWatched ? `w-16` : `w-24`} rounded-md`}
+      />
       <div className="flex justify-between w-full">
         <div>
           <h2 className="text-lg font-bold mt-2">{Title}</h2>
