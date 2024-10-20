1175eda73424f1b091cb2c6c70a78e72ceadaea2
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-20 12:55:16 +0100

    add focus to search bar on keydown event

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Icon from "../assets/movie-buff.svg";
 import IconSmall from "../assets/icon-small.svg";
 import PropTypes from "prop-types";
+import { useEffect, useRef } from "react";
 
 Navbar.propTypes = {
   movies: PropTypes.array,
@@ -8,10 +9,22 @@ Navbar.propTypes = {
 };
 
 export default function Navbar({ movies, setTitle }) {
+  const inputEl = useRef(null);
+
   // handle Enter key press
   const handleKeyPress = (e) => {
     if (e.key === "Enter") setTitle(e.target.value.trim());
   };
+
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, []);
 
   return (
     <nav className="bg-slate-900 p-2 fixed top-0 w-full z-50 shadow-md">
@@ -33,8 +46,9 @@ export default function Navbar({ movies, setTitle }) {
         <input
           type="text"
           placeholder="Search for movies..."
-          className="p-2 w-1/2 md:w-1/3 bg-gray-100 text-gray-700 rounded-md"
+          className="p-2 w-1/2 md:w-1/3 bg-gray-100 text-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all duration-300 ease-in-out"
           onKeyDown={handleKeyPress}
+          ref={inputEl}
         />
         <p className="text-gray-100 text-sm md:text-xl font-thin">
           Found <span className="font-bold text-gray-500">{movies.length}</span>{" "}
